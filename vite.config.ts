c4e90cecaf62f3cc4078a8ce4e75c268d4d8015a
DuRuofu <duruofu@qq.com>
2024-05-01 14:50:53 +0800

    feat(env): update env config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { viteMockServe } from 'vite-plugin-mock';
 import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
+
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
 	// 获取环境变量
